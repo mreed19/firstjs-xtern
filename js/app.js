79a5f98150a81977afcfd30c16d012fe2a8723bb
Michael Reed <michael.reed315@gmail.com>
2016-06-01 13:50:21 -0400

    Moved the dl to be added dynamically in javascript to allow addition of multiple entries

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 $(document).foundation()
+  var myForm = document.querySelector('form');
+  myForm.onsubmit = function(ev) {
+    ev.preventDefault();
+    var details = document.querySelector('.details');
+
+    var firstName = this.firstName.value;
+    var hairColor = this.hairColor.value;
+    var age = this.age.value;
+    var birthplace = this.birthplace.value;
+
+    var colorDiv = '<div style="height: 100px; width: 100px; background-color: ' + hairColor + '"></div>'
+
+    details.innerHTML +=  ' \
+      <dl> \
+          <dt>First Name</dt> \
+          <dd>' + firstName + '</dd> \
+          <dt>Hair Color</dt> \
+          <dd>' + colorDiv + '</dd> \
+          <dt>Age</dt> \
+          <dd>' + age + '</dd> \
+          <dt>Birthplace</dt> \
+          <dd>' + birthplace + '</dd> \
+        </dl>';
+  };
