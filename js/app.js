db40f755e4d4e854a71362ac2ffe1b188a641268
Michael Reed <michael.reed315@gmail.com>
2016-06-01 15:28:44 -0400

    Build DL as a DOM element and wrap in and IIFE to avoid global variables

--- js/app.js
+++ js/app.js
@@ -1,35 +1,40 @@
-$(document).foundation()
+$(document).foundation();
 
-var buildColorDiv = function(color) {
-  return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>';
-};
+(function() {
 
-var buildList = function(listValues) {
-  return '<dl>' +
-    buildListItem('First Name', listValues.firstName) +
-    buildListItem('Hair Color', listValues.hairColor) +
-    buildListItem('Age', listValues.age) +
-    buildListItem('Birthplace', listValues.birthplace) +
-    '</dl>';
-};
-
-var buildListItem = function(term, definition) {
-  return '<li><dt>' + term + '</dt><dd>' + definition + '</dd></li>';
-};
-
-var addValuesToDetails = function(ev) {
-  ev.preventDefault();
-  var details = document.querySelector('.details');
-
-  var listValues = {
-    firstName: this.firstName.value,
-    hairColor: buildColorDiv(this.hairColor.value),
-    age: this.age.value,
-    birthplace: this.birthplace.value
+  var buildColorDiv = function(color) {
+    return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>';
   };
 
-  details.innerHTML += buildList(listValues);
-};
+  var buildList = function(listValues) {
+    var dl = document.createElement('dl');
+    dl.style.border = '1px solid red';
+    dl.innerHTML= buildListItem('First Name', listValues.firstName) +
+      buildListItem('Hair Color', listValues.hairColor) +
+      buildListItem('Age', listValues.age) +
+      buildListItem('Birthplace', listValues.birthplace);
 
-var myForm = document.querySelector('form');
-myForm.onsubmit = addValuesToDetails;
+    return dl;
+  };
+
+  var buildListItem = function(term, definition) {
+    return '<li><dt>' + term + '</dt><dd>' + definition + '</dd></li>';
+  };
+
+  var addValuesToDetails = function(ev) {
+    ev.preventDefault();
+    var details = document.querySelector('.details');
+
+    var listValues = {
+      firstName: this.firstName.value,
+      hairColor: buildColorDiv(this.hairColor.value),
+      age: this.age.value,
+      birthplace: this.birthplace.value
+    };
+
+    details.appendChild(buildList(listValues));
+  };
+
+  var myForm = document.querySelector('form');
+  myForm.onsubmit = addValuesToDetails;
+})();
